9c3fc5488e4f51c365da6b6e331fc74b6234366a
song_xiaohui <song_xiaohui@outlook.com>
2018-06-18 17:14:02 +0800

    jdk8 Stream特性

---
+++ src/jdk8/Jdk8Stream.java
@@ -0,0 +1,14 @@
+package jdk8;
+
+import java.util.ArrayList;
+
+public class Jdk8Stream {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		list.stream().filter(x -> x.equals("2")).forEach(System.out::println);
+	}
+}
