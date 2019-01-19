2902f1dbf16232c4fd0c529e1e2edd038c18f2cc
songxiaohui <song_xiaohui@outlook.com>
2019-01-19 21:28:46 +0800

    利用枚举创建单例

---
+++ src/com/goodlike/song/singleton/SingletonDemo3.java
@@ -0,0 +1,19 @@
+package com.goodlike.song.singleton;
+
+/**
+ * @author Song Yinghui
+ * @date 2019/1/19
+ * @Version 1.0
+ * @Desc 枚举方式创建单例
+ */
+public enum SingletonDemo3 {
+    //枚举的域（field）本身就是 SingletonDemo3的实例对象
+    INSTANCE;
+    public SingletonDemo3 getInstance(){
+        return INSTANCE ;
+    }
+
+    public void test(){
+
+    }
+}
